5645e0d26c9a58ad0961f04ab16de12b1feaed86
Erik Haugervåg Hjulstad <erikhhju@gmail.com>
2023-02-16 17:13:18 +0100

    auto generated index.html

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const url = "https://valorant-api.com/v1/weapons";
+const main = document.querySelector(".container");
+const bopdy = document.querySelector("body");
+
+
+const getData = async () => {
+    try {
+        const res = await fetch(url);
+        const results = await res.json();
+        console.log(results);
+        
+        results.data.forEach((obj) => {
+            const div = document.createElement("div");
+            div.classList.add("card");
+    
+    
+            const cardImg = document.createElement("img");
+            cardImg.src = obj.displayIcon;
+            const cardTitle = document.createElement("h3");
+            cardTitle.innerHTML = obj.displayName
+            const cardCost = document.createElement("h5");
+            cardCost.innerHTML = `Cost: ${obj.shopData.cost}` 
+            const stats = document.createElement("div");
+            stats.innerHTML = `<h5>Equip-time: ${obj.weaponStats.equipTimeSeconds}s</h5>
+            <h5>Fire-Rate: ${obj.weaponStats.fireRate} rps</h5>
+            <h5>First Bullet Accuracy: ${obj.weaponStats.firstBulletAccuracy}</p>
+            <h5>Magazine Size: ${obj.weaponStats.magazineSize}</h5>
+            <h5>Reload-time: ${obj.weaponStats.reloadTimeSeconds}s</h5>`
+            
+            
+            
+            div.appendChild(cardTitle);
+            div.appendChild(cardImg);
+            div.appendChild(cardCost);
+            div.appendChild(stats);
+            main.appendChild(div);
+    
+    
+        })
+    } catch (error) {
+        console.log(error)
+        const errorMsg = document.createElement("h2");
+        errorMsg.classList.add("error");
+        errorMsg.innerHTML = "An error occurred while fetching data, sorry for the inconvenience.";
+        main.appendChild(errorMsg);
+        
+
+    }
+   
+}
+getData();
