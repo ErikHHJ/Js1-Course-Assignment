005ab06fa94389c2e306525dc200418f9c5d349f
ErikHHJ <erikhhju@gmail.com>
2023-02-17 13:03:50 +0100

    optimized details page, css needs more work

--- js/details.js
+++ js/details.js
@@ -15,25 +15,40 @@ const detailsCard = async () => {
         const results = await res.json();
         console.log(results)
 
-        Object.keys(results).forEach(() => {
+        const data = results.data
             const detailsImg = document.createElement("img");
-            detailsImg.src = results.data.displayIcon
+            detailsImg.src = data.displayIcon
             const detailsTitle = document.createElement("h1");
-            detailsTitle.innerHTML = results.data.displayName;
+            detailsTitle.innerHTML = data.displayName;
+            console.log("something")
+            const detailsCost = document.createElement("h5");
+            detailsCost.innerHTML = `Cost: ${data.shopData.cost}`
 
-           /* const detailsStats = document.createElement("div");
-            detailsStats.innerHTML = `<h5>Equip-time: ${results.data.weaponStats.equipTimeSeconds}s</h5>
-            <h5>Fire-Rate: ${results.data.weaponStats.fireRate} rps</h5>
-            <h5>First Bullet Accuracy: ${results.data.weaponStats.firstBulletAccuracy}</p>
-            <h5>Magazine Size: ${results.data.weaponStats.magazineSize}</h5>
-            <h5>Reload-time: ${results.data.weaponStats.reloadTimeSeconds}s</h5>`*/
-            
+            const detailsStats = document.createElement("div");
+            detailsStats.innerHTML = `<h5>Equip-time: ${data.weaponStats.equipTimeSeconds}s</h5>
+            <h5>Fire-Rate: ${data.weaponStats.fireRate} rps</h5>
+            <h5>First Bullet Accuracy: ${data.weaponStats.firstBulletAccuracy}</p>
+            <h5>Magazine Size: ${data.weaponStats.magazineSize}</h5>
+            <h5>Reload-time: ${data.weaponStats.reloadTimeSeconds}s</h5>`
+
+            const div = document.createElement("div");
+            div.classList.add("flexdiv");
+            const divTitle = document.createElement("h5");
+            const divImg = document.createElement("img");
+            const category = document.createElement("h5");
+            category.innerHTML = `Category: ${data.shopData.category}`
+
+            divTitle.innerHTML = "Killfeed Icon:"
+            divImg.src = data.killStreamIcon
+
+            div.appendChild(divTitle);
+            div.appendChild(divImg);
             container.appendChild(detailsTitle);
+            container.appendChild(category);
             container.appendChild(detailsImg);
+            container.appendChild(detailsCost);
             container.appendChild(detailsStats);
-            
-
-        })
+            container.appendChild(div);
         
     }
     catch(error){
